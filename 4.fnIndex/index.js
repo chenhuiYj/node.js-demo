02fef1bea235096360d48b764d468c4584b681fe
chui-web <158874645@qq.com>
2018-12-05 15:45:30 +0800

    fnIndex 添加注释

--- 4.fnIndex/index.js
+++ 4.fnIndex/index.js
@@ -21,13 +21,14 @@ fs.readFile('./4.fnIndex/fn.js', (err, data) => {
 
 fs.readFile('./README.md', (err, data) => {
     if (err) {
-        //如 果 出 错， 输 出 错 误 日 志， 并 给 客 户 端 返 回“ Server Error”
         console.error(err);
     }
     else {
+        //根据换行符分割
         let _data=data.toString().split('\r\n');
         let i=1;
         _data.forEach((item,index)=>{
+            //如果找到 %% 就更换为索引
             if(/%%/.test(item)){
                 _data[index]=item.replace('%%',i+'.');
                 i++;
