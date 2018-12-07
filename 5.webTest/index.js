d7ebdbb3556edf8eb8e2eb7ab4c07004833a90e6
chui-web <158874645@qq.com>
2018-12-07 13:36:20 +0800

    修改 5.webTest demo

--- 5.webTest/index.js
+++ 5.webTest/index.js
@@ -1,14 +1,32 @@
 const express=require('express');
 const app=express();
-const port=3000;
+//const port=3000;
+const articles = [{ title: 'Example' },{ title: 'Example2' }];
 app.get('/',(req,res)=>{
     res.send('Hello Node');
 })
 
 app.get('/list',(req,res)=>{
-    res.send('this is list');
+    res.send(articles);
 })
 
-app.listen(port,()=>{
-    console.log('express',port);
-})
+app.get('/list/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('Fetching:', id);
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('Deleting:', id);
+    delete articles[id];
+    res.send({ message: 'Deleted' });
+});
+
+app.listen(3000);
+
+module.exports = app;
+
+// app.listen(port,()=>{
+//     console.log('express',port);
+// })
