7273e86aa3230d3068aab16214b8bfd1a39c6e3d
chui-web <158874645@qq.com>
2019-01-07 15:55:37 +0800

    删除多余代码

--- 4.fnIndex/index.js
+++ 4.fnIndex/index.js
@@ -1,23 +1,4 @@
 let fs = require('fs');
-let fns=[];
-fs.readFile('./4.fnIndex/fn.js', (err, data) => {
-    if (err) {
-        //如 果 出 错， 输 出 错 误 日 志， 并 给 客 户 端 返 回“ Server Error”
-        console.error(err);
-    }
-    else {
-        let md="";
-        let obj=new Function(data.toString().replace('let','let _ecObj=')+';return _ecObj')();
-        Object.keys(obj).forEach((item,index)=>{
-            //md+=`## ${index+1}.${item}\r\n`
-            fns.push({
-                index:index,
-                name:item
-            })
-        })
-        //fs.writeFileSync("./README.md", md);
-    }
-})
 
 fs.readFile('./README_TEST.md', (err, data) => {
     if (err) {
